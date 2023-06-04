9852f3ea4f30dae46bd44e0448eeb44aab1da130
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-04 15:58:39 +0200

    Adds dependency injection container for PostgresqlTaskRepository and PostgresqlImageRepository

--- src/dependency.injection.ts
+++ src/dependency.injection.ts
@@ -1,6 +1,11 @@
 import "reflect-metadata";
-
 import { Container } from "typedi";
 
+import { TOKENS } from "./domain/shared/constants";
+import PostgresqlTaskRepository from "./domain/task/infrastructure/PostgresqlTaskRepository";
+import PostgresqlImageRepository from "./domain/image/infrastructure/PostgresqlImageRepository";
+
+Container.set(TOKENS.TASK_REPOSITORY, PostgresqlTaskRepository);
+Container.set(TOKENS.IMAGE_REPOSITORY, PostgresqlImageRepository);
 
 export default Container;
