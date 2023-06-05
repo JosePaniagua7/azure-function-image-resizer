2710adeef09b21ebaa4d135c7e85aea2552d0e6d
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-05 13:35:19 +0200

    Decouple code

--- src/domain/image/application/ImageCreator.ts
+++ src/domain/image/application/ImageCreator.ts
@@ -16,35 +16,57 @@ export default class ImageCreator {
         this.resizer = Container.get(TOKENS.IMAGE_RESIZER);
     }
 
+    private async createVariations(source: ImageCreationPayload, taskId: string) {
+        const creationPromises = requiredSizes.map((size) => this.repository.create({
+            status: 'created',
+            originalResourcePath: source.path,
+            originalName: source.originalName,
+            mimeType: source.mimeType,
+            md5: source.md5,
+            width: size,
+            heigth: size,
+            taskId,
+        }))
+        return Promise.all(creationPromises);
+    }
+
+    private async requestVariationResize(variations: any): Promise<string[]> {
+        return variations.map((variation: any) => this.resizer.resize(
+            variation.originalResourcePath,
+            variation.width,
+            variation.originalName
+        ));
+    }
+
+    private async setGeneratingStatusToVariations(variations: any[], status: string) {
+        const updatePromises = variations.map((variation) => {
+            variation.status = 'generating';
+            return variation.save();
+        });
+        return Promise.all(updatePromises);
+    }
+
+    private async handleResizingResponse(variations: any, resizingResponse: PromiseSettledResult<string>[]) {
+        const updateImagesPromises = resizingResponse.map((settledPromiseResult: PromiseSettledResult<string>, i: number) => {
+            const curreentImageProcessing = variations[i];
+            curreentImageProcessing.status = settledPromiseResult.status === 'fulfilled' ? 'success' : 'error';
+            if (settledPromiseResult.status === 'fulfilled' && curreentImageProcessing.status === 'success')
+                curreentImageProcessing.resizedResourcePath = settledPromiseResult.value;
+
+            return curreentImageProcessing.save();
+        })
+        return Promise.all(updateImagesPromises);
+    }
+
     async create(source: ImageCreationPayload, taskId: string) {
         try {
-            const creationPromises = requiredSizes.map((size) => this.repository.create({
-                status: 'created',
-                originalResourcePath: source.path,
-                originalName: source.originalName,
-                mimeType: source.mimeType,
-                md5: source.md5,
-                width: size,
-                heigth: size,
-                taskId,
-            }))
-            const imageVariations = await Promise.all(creationPromises);
+            const imageVariations = await this.createVariations(source, taskId);
             console.log('Image variations created: ', imageVariations.length);
             // Once we have the variations in our scope, we can send a request to resize the images
-            const resizingOperations = imageVariations.map((variation) => this.resizer.resize(
-                variation.originalResourcePath,
-                variation.width,
-                variation.originalName
-            )
-            );
+            const resizingOperations = await this.requestVariationResize(imageVariations);
             console.log('Resize operations created: ', resizingOperations.length);
             // After that, we can update the variations status to generating
-            const statusGenerationUpdate = imageVariations.map((variation) => {
-                variation.status = 'generating';
-                return variation.save();
-            })
-            // Let's wait for db update 
-            await Promise.all(statusGenerationUpdate);
+            await this.setGeneratingStatusToVariations(imageVariations, 'generating');
             console.log('image status updated');
 
             // Now, let's wait for all the resizing operations to finish
@@ -52,13 +74,7 @@ export default class ImageCreator {
             console.log('Resizing operations is done: ', resizingOperationsResult);
 
             // Let's update image based on the promise status
-            const resizedImageVariations = resizingOperationsResult.map((settledPromiseResult, i) => {
-                const curreentImageProcessin = imageVariations[i];
-                curreentImageProcessin.status = settledPromiseResult.status === 'fulfilled' ? 'success' : 'error';
-                if (settledPromiseResult.status === 'fulfilled' && curreentImageProcessin.status === 'success')
-                    curreentImageProcessin.resizedResourcePath = settledPromiseResult.value;
-                return curreentImageProcessin.save();
-            })
+            const resizedImageVariations = await this.handleResizingResponse(imageVariations, resizingOperationsResult);
             return Promise.all(resizedImageVariations);
         } catch (e) {
             console.warn('Error while calling create method in repository: ', e);
