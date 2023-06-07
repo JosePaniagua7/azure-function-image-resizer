888f68964c027275fb7dee373a34d8a276d964fc
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-07 22:08:57 +0200

    Reuse utils in local image resizer

--- src/domain/image/application/LocalImageResizer.ts
+++ src/domain/image/application/LocalImageResizer.ts
@@ -1,7 +1,8 @@
 import sharp from "sharp";
 import ImageResizerService from "../contracts/ImageResizerService";
-import { createHash } from "crypto";
 import { mkdir } from "fs/promises";
+import { extractFileExtension, getHash } from "../../shared/utils";
+import { getDestinationPath } from "../utils";
 
 export default class LocalImageResizer implements ImageResizerService {
     outputDir: string;
@@ -9,45 +10,18 @@ export default class LocalImageResizer implements ImageResizerService {
         this.outputDir = './output'
     }
 
-    private getHash(path: string, dimension: number, originalName: string) {
-        return createHash('md5')
-            .update(JSON.stringify({ path, dimension, originalName }))
-            .digest('hex');
-    }
-
-    private extractFileName(originalName: string) {
-        return originalName.split('.').shift();
-    }
-
-    private extractFileExtension(originalName: string) {
-        return originalName.split('.').pop();
-    }
-
-    private getDestinationPath(dimension: number, originalName: string) {
-        const nameWithoutExtension = this.extractFileName(originalName);
-        return `${this.outputDir}/${nameWithoutExtension}/${dimension}`;
-    };
-
-    private async createDestinationFolder(path: string): Promise<void> {
-        await mkdir(path, { recursive: true });
-    }
-
     async resize(path: string, dimension: number, originalName: string): Promise<string> {
-        const destinationPath = this.getDestinationPath(dimension, originalName);
-        const operationHash = this.getHash(path, dimension, originalName);
-        const fileExtension = this.extractFileExtension(originalName);
+        const destinationPath = getDestinationPath(dimension, originalName);
+        const operationHash = getHash(JSON.stringify({ path, dimension, originalName }));
+        const fileExtension = extractFileExtension(originalName);
 
-        await this.createDestinationFolder(destinationPath);
+        await mkdir(destinationPath, { recursive: true });
 
         const resourcePath = `${destinationPath}/${operationHash}.${fileExtension}`;
 
         return sharp(path)
             .resize({ width: dimension, height: dimension })
             .toFile(resourcePath)
-            .then((result) => {
-                // console.log('resizing result: ');
-                // console.log(result);
-                return resourcePath;
-            })
+            .then(() => resourcePath)
     };
 }
