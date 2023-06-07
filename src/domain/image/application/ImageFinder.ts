cb578d2fbb00faa8820e847cb41e4089dbdecb98
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-07 22:57:12 +0200

    Implements image finder use case

---
+++ src/domain/image/application/ImageFinder.ts
@@ -0,0 +1,18 @@
+
+import { ImageRepository } from "../contracts/ImageRepository";
+import Container from '../../../dependency.injection';
+import { TOKENS } from '../../shared/constants';
+import ImageException from "../exceptions/ImageException";
+
+export default class ImageFinder {
+    repository: ImageRepository;
+    constructor() {
+        this.repository = Container.get(TOKENS.IMAGE_REPOSITORY);
+    }
+
+    async findById(id: string) {
+        const image = await this.repository.findByPk(id);
+        if (!image) throw ImageException.imageNotFound(id);
+        return image;
+    }
+}
