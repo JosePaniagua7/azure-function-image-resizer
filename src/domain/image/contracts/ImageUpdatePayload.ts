dd5eb6c6fe098b4542914c558ee4d54ecda45384
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-07 22:57:40 +0200

    Defines interface for ImageUpdatePayload

---
+++ src/domain/image/contracts/ImageUpdatePayload.ts
@@ -0,0 +1,7 @@
+import { UploadedFile } from "../../shared/contracts/UploadedFile";
+
+export interface ImageUpdatePayload {
+    id: string;
+    dimension: number;
+    resizedImage: UploadedFile[]
+}
