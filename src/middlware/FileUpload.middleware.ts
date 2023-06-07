fd4c80755b14e8da7799c70c7eeac90acdf63e8d
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-07 22:09:13 +0200

    Adds resizedImage into middleware

--- src/middlware/FileUpload.middleware.ts
+++ src/middlware/FileUpload.middleware.ts
@@ -5,5 +5,6 @@ const upload = multer({ dest: "uploads/" });
 
 export const uploadFiles = (req: Request, res: Response, next: NextFunction) =>
   upload.fields([
-    { name: "resource", maxCount: 1 }
+    { name: "resource", maxCount: 1 },
+    { name: "resizedImage", maxCount: 1 },
   ])(req, res, next);
