f4067eb8b45c70aedd3a0398357ef697fa7f4aca
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-08 18:01:24 +0200

    Azure image resizer connecting with local host successfully

---
+++ src/domain/image/application/AzureImageResizer.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance } from "axios";
+import { readFile } from "fs/promises";
+
+import ImageResizerService from "../contracts/ImageResizerService";
+import { TOKENS } from "../../shared/constants";
+import Container from '../../../dependency.injection';
+
+
+export default class AzureImageResizer implements ImageResizerService {
+    http: AxiosInstance
+    constructor() {
+        // IMPORTANT! THIS VARIABLES SHOULD BE INJECTED BY ENV PROVIDER, DUE SOME 
+        // SPEED UP TO DELIVER TECHNICAL CHALLENGE ASAP, THIS WILL BE SKIPED
+        this.http = axios.create({
+            proxy: false,
+            baseURL: 'http://127.0.0.1:7071/api/resize-image/',
+            // baseURL: 'https://jose-paniagua-image-resizer.azurewebsites.net/api/resize-image/',
+        })
+    }
+
+    async resize(imageId: string, path: string, dimension: number, originalName: string): Promise<string> {
+        try {
+            const fileBuffer = await readFile(path);
+            const form = new FormData();
+            form.append('resource', new Blob([fileBuffer]));
+            const endpoint = `${imageId}?code=${Container.get(TOKENS.AZURE_CODE)}&dimensions=${dimension}`;
+            return this.http.post(endpoint, form);
+        } catch (e) {
+            console.log('e: ', e);
+        } finally {
+            return ''
+        }
+    };
+}
