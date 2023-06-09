90e8ade8cc8c8703befd22a3e04974c2cfcc0b3f
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-09 22:44:18 +0200

    Adds support to create database if not exists

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,22 @@ import dotenv from "dotenv";
 
 import app from "./app";
 import connection from "./models/connection";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
 
 console.log("Starts connecting with db");
 
-connection.sync({ force: true }).then(() => {
-  console.log("sequelize sync successful");
-  app.listen(app.get("port"), () => {
-    console.log(
-      "  App is running at http://localhost:%d in %s mode",
-      app.get("port"),
-      app.get("env")
-    );
-    console.log("  Press CTRL-C to stop\n");
+connection().then((conn: Sequelize) => {
+  conn.sync({ force: true }).then(() => {
+    console.log("sequelize sync successful");
+    app.listen(app.get("port"), () => {
+      console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+      );
+      console.log("  Press CTRL-C to stop\n");
+    });
   });
-});
+})
