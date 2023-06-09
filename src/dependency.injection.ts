e7246f5db54ae4908d65924ab4e4bd918f8d5943
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-09 22:44:01 +0200

    Run eslint over files

--- src/dependency.injection.ts
+++ src/dependency.injection.ts
@@ -1,13 +1,27 @@
 import "reflect-metadata";
+
 import { Container } from "typedi";
 
+import AzureImageResizer from "./domain/image/application/AzureImageResizer";
+import LocalImageResizer from "./domain/image/application/LocalImageResizer";
+import PostgresqlImageRepository from "./domain/image/infrastructure/PostgresqlImageRepository";
 import { TOKENS } from "./domain/shared/constants";
 import PostgresqlTaskRepository from "./domain/task/infrastructure/PostgresqlTaskRepository";
-import PostgresqlImageRepository from "./domain/image/infrastructure/PostgresqlImageRepository";
-import LocalImageResizer from "./domain/image/application/LocalImageResizer";
+
+// const isProd = process.env.NODE_ENV === 'production';
+const isProd = true;
 
 Container.set(TOKENS.TASK_REPOSITORY, PostgresqlTaskRepository);
 Container.set(TOKENS.IMAGE_REPOSITORY, PostgresqlImageRepository);
-Container.set(TOKENS.IMAGE_RESIZER, new LocalImageResizer());
+Container.set(
+  TOKENS.IMAGE_RESIZER,
+  isProd ? new AzureImageResizer() : new LocalImageResizer()
+);
+Container.set(
+  TOKENS.AZURE_CODE,
+  isProd
+    ? "HjbLPb0S71628Zzj41OEpDhaMctR0H7EupI1Sosq-x_KAzFuk2Hxrw=="
+    : "AZURE_TEST_CODE"
+);
 
 export default Container;
