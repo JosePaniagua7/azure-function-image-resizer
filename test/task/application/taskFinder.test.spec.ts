0bc64b3d2cdb37b6de85f2116635f75444643ee3
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-11 04:26:54 +0200

    implements test for test finder

---
+++ test/task/application/taskFinder.test.spec.ts
@@ -0,0 +1,26 @@
+import TaskFinder from '../../../src/domain/task/application/TaskFinder';
+import { tasksSources } from '../../../src/domain/task/infrastructure/TestTaskRepository';
+
+describe("Test image finder use case", () => {
+    it("find by id should return a new instance of task repository", async () => {
+        const finder = new TaskFinder();
+        const expectedProperties = tasksSources[0];
+
+        const returnedTask = await finder.findById('1');
+
+        expect(returnedTask.id).toBe(expectedProperties.id);
+        expect(returnedTask.status).toBe(expectedProperties.status);
+        expect(returnedTask.resourcePath).toBe(expectedProperties.resourcePath);
+        expect(finder.repository.findByPk).toBeCalled()
+    });
+
+
+    it("find by id should return a new instance of task repository", async () => {
+        const finder = new TaskFinder();
+
+        const tasks = await finder.list();
+
+        expect(tasks.length).toBe(tasksSources.length);
+        expect(finder.repository.findAll).toBeCalled()
+    });
+});
