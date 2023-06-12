14bb711e67e5115b7a9333ed91675e7528d51f40
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-12 12:44:28 +0200

    Implements test

--- test/task/application/taskUpdater.test.spec.ts
+++ test/task/application/taskUpdater.test.spec.ts
@@ -1,5 +1,8 @@
 import TaskUpdater from '../../../src/domain/task/application/TaskUpdater';
-
+import TestTaskRepository from '../../../src/domain/task/infrastructure/TestTaskRepository';
+import TaskException from '../../../src/domain/task/exceptions/TaskException';
+import Container from '../../../src/dependency.injection';
+import { TOKENS } from '../../../src/domain/shared/constants';
 
 describe("Test task updater use case", () => {
     it("Updater should call finder", async () => {
@@ -9,6 +12,37 @@ describe("Test task updater use case", () => {
         await updater.imageResized('1');
 
         expect(finderSpy).toBeCalled();
+    });
+
+    it("Updater should capture exception and call couldNotProcessTaskUpdate", async () => {
+        const updater = new TaskUpdater();
+
+        updater.finder.findById = jest.fn((id: string): Promise<any> => Promise.resolve({
+            id: '1',
+            createdAt: '',
+            status: 'CREATED',
+            resourcePath: 'test',
+            $get() {
+                throw new Error('Error connecting to db');
+            }
+        }));
+
+        try {
+            await updater.imageResized('1');
+            expect(true).toBe(false);
+        } catch (e: TaskException | any) {
+            expect(e.message).toBe('Could not process task 1 resizing notification');
+        }
+
+
+        // await updater.imageResized('1');
+
+        // expect(async () => {
+        //     await updater.imageResized('1');
+        // }).toThrowError(TaskException)
+
+
+        // expect(finderSpy).toBeCalled();
     });
 
 
