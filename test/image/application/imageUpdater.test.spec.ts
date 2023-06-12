e248b293098a4b567b78957f3bffaf40fe7caea9
José Paniagua <jose.paniagua.gomez@gmail.com>
2023-06-12 11:48:54 +0200

    Fix input file for testss

---
+++ test/image/application/imageUpdater.test.spec.ts
@@ -0,0 +1,30 @@
+import ImageUpdater from '../../../src/domain/image/application/ImageUpdater';
+import { imagesSources } from '../../../src/domain/image/infrastructure/TestImageRepository';
+import * as utils from '../../../src/domain/shared/utils';
+
+describe("Test image updater use case", () => {
+    it("Sould call path resolvers", async () => {
+        const updater = new ImageUpdater();
+        const fileExtensionSpy = jest.spyOn(utils, 'extractFileExtension');
+        const hashSpy = jest.spyOn(utils, 'getHash');
+
+        await updater.resizeOperationReceived({
+            id: '1',
+            resizedImage: [{
+                originalname: 'test',
+                mimetype: 'jpg',
+                filename: 'technicalInterview',
+                fieldname: 'resource',
+                encoding: 'md5',
+                destination: 'output',
+                path: './test/resources/technicalInterviewInput.jpeg',
+                size: 20,
+            }],
+            dimension: 800,
+        });
+
+
+        expect(fileExtensionSpy).toBeCalled();
+        expect(hashSpy).toBeCalled();
+    });
+});
